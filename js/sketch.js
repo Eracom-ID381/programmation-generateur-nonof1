0a00b241b0723c06f66e53ded56bca2c22fb838c
Simon Pinkas <simon@code-think.com>
2020-05-13 14:49:16 +0200

    Fix negative roofs

--- js/sketch.js
+++ js/sketch.js
@@ -13,7 +13,7 @@ function draw() {
     // et si le timer est écoulé
     if (frameCount % (timer * 60) == 0 && !mouseIsPressed) {
         // Appel de notre fonction generator
-        generator(random(0, windowWidth), random(0, windowHeight), random(0, 200), random(0, 400), random(0, windowHeight));
+        generator(random(0, windowWidth), random(0, windowHeight), random(0, 200), random(0, 400), random(0, 400));
     }
 }
 
@@ -22,10 +22,11 @@ function generator(x, y, largeur, hauteur, hauteurToit) {
     // Façade
     rect(x, y, largeur, hauteur);
     //Toit
-    triangle(x - largeur / 2, y - hauteur / 2, x + largeur / 2, y - hauteur / 2, x, hauteur - hauteurToit);
+    triangle(x - largeur / 2, y - hauteur / 2, x + largeur / 2, y - hauteur / 2, x, y - hauteur/2 - hauteurToit);
     //Porte
     rect(x + largeur / 3, y, largeur / 4, hauteur / 4);
     rect(x - largeur / 3, y, largeur / 4, hauteur / 4);
+    console.log(hauteur / 2);
 
 }
 
