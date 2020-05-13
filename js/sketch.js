c38775cdb87676129292b330f37f161db2273751
nonof1 <60931115+nonof1@users.noreply.github.com>
2020-05-13 14:22:44 +0200

    Facade et toit fait
    - Toit qui se met à l'envers???

--- js/sketch.js
+++ js/sketch.js
@@ -4,34 +4,33 @@
 let timer = 1;
 
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
-  rectMode(CENTER);
+    createCanvas(window.innerWidth, window.innerHeight);
+    rectMode(CENTER);
 }
 
 function draw() {
-  // Conditionellle nous permettant de savoir si la souris est appuyée 
-  // et si le timer est écoulé
-  if (frameCount % (timer * 60) == 0 && !mouseIsPressed) {
-    // Appel de notre fonction generator
-    generator(width / 2, height / 2, height-150);
-  }
+    // Conditionellle nous permettant de savoir si la souris est appuyée
+    // et si le timer est écoulé
+    if (frameCount % (timer * 60) == 0 && !mouseIsPressed) {
+        // Appel de notre fonction generator
+        generator(random(0, windowWidth), random(0, windowHeight), 100, 120, random(0, windowHeight));
+    }
 }
 
 // Fonction avec quatres paramètres
-function generator(x, y, size) {
-  // fill utilise le paramètre de notre fonction
-  for (let offset = size; offset > 0; offset -= 10) { 
-      fill(random(0,230));
-      rect(x, y, offset, offset);
-      console.log(offset);
-  }
+function generator(x, y, largeur, hauteur, hauteurToit) {
+    // Façade
+    rect(x, y, largeur, hauteur);
+    //Toit
+    triangle(x - largeur / 2, y - hauteur / 2, x + largeur / 2, y - hauteur / 2, x, hauteurToit);
+
 }
 
-function mousePressed() { 
-  // Fonction de librairie P5 nous permettant de sauvegarder une image du canvas
-  save();
+function mousePressed() {
+    // Fonction de librairie P5 nous permettant de sauvegarder une image du canvas
+    save();
 }
 
-function windowResized() { 
+function windowResized() {
     resizeCanvas(window.innerWidth, window.innerHeight);
 }
